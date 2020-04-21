5002c4578a0a3470b9457df2c5f4ea0025b4a671
yangtau <yanggtau@gmail.com>
2020-04-21 16:06:53 +0800

    add while; modify for

--- src/ast_node.h
+++ src/ast_node.h
@@ -10,6 +10,7 @@ enum ast_node_type {
     AST_NODE_CALL,
     AST_NODE_FUNC,
     AST_NODE_RETURN,
+    AST_NODE_WHILE,
     // type only node:
     AST_NODE_BREAK,
     AST_NODE_CONTINUE,
@@ -41,6 +42,7 @@ enum ast_node_op_type {
     AST_NODE_OP_MUL,
     AST_NODE_OP_DIV,
     AST_NODE_OP_MOD,
+    AST_NODE_OP_DDOT,
 };
 
 struct ast_node_op {
@@ -61,11 +63,14 @@ struct ast_node_func {
     struct ast_node* stats;
 };
 
-struct ast_node_for {
-    struct ast_node* init;
+struct ast_node_while {
     struct ast_node* cond;
-    struct ast_node* update;
     struct ast_node* stats;
+};
+
+struct ast_node_for {
+    struct ast_node* vars;
+    struct ast_node* iterator;
 };
 
 struct ast_node_call {
@@ -124,14 +129,16 @@ struct ast_node* ast_node_assign_new(struct ast_node* vars,
  */
 struct ast_node* ast_node_return_new(struct ast_node* expr);
 
-/** ast_node_for_new:
- * `for cond {}`: `init` and `update` are NULL
- * `for init; cond; update {}`:
- * `for {}`: `init`, `update` and `cond` are NULL
+/** ast_node_while_new:
  */
-struct ast_node* ast_node_for_new(struct ast_node* init, struct ast_node* cond,
-                                  struct ast_node* update,
+struct ast_node* ast_node_while_new(struct ast_node* cond,
+                                    struct ast_node* stats);
+/** ast_node_for_new:
+ */
+struct ast_node* ast_node_for_new(struct ast_node* vars,
+                                  struct ast_node* iterator,
                                   struct ast_node* stats);
+
 /** ast_node_loopctrl_new:
  * @type: AST_NODE_BREAK or AST_NODE_CONTINUE
  */
@@ -145,4 +152,6 @@ struct ast_node* ast_node_str_len_new(const char* s, size_t len);
 struct ast_node* ast_node_bool_new(int v);
 struct ast_node* ast_node_id_new(const char* s);
 struct ast_node* ast_node_nil_new();
+struct ast_node* ast_node_list_new(struct ast_node* args);
+struct ast_node* ast_node_tuple_new(struct ast_node* args);
 #endif // _HG_AST_NODE_H_
