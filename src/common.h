2771ba86fc739bb05a4d80262572815eabcc6341
yangtau <yanggtau+github@gmail.com>
2020-06-01 20:24:42 +0800

    add backtrace

--- src/common.h
+++ src/common.h
@@ -7,19 +7,30 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <execinfo.h>
+#include <unistd.h>
+
+#define _force_inline_ inline __attribute__((always_inline))
+static _force_inline_ void print_backtrace() {
+    void* callstack[128];
+    size_t frames = backtrace(callstack, 128);
+    backtrace_symbols_fd(callstack, frames, STDERR_FILENO);
+}
 
 #define unimplemented_(...)                                           \
     do {                                                              \
         fprintf(stderr, "unimplemented in function %s:\n", __func__); \
         fprintf(stderr, __VA_ARGS__);                                 \
-        fprintf(stderr, "\n");                                \
+        fprintf(stderr, "\n");                                        \
+        print_backtrace();                                            \
         exit(-1);                                                     \
     } while (0)
 
-#define unreachable_()                                                   \
-    do {                                                                 \
+#define unreachable_()                                             \
+    do {                                                           \
         fprintf(stderr, "unreachable in function %s\n", __func__); \
-        exit(-1);                                                        \
+        print_backtrace();                                         \
+        exit(-1);                                                  \
     } while (0)
 
 #define error_(...)                                           \
@@ -27,9 +38,8 @@
         fprintf(stderr, "error in function %s:\n", __func__); \
         fprintf(stderr, __VA_ARGS__);                         \
         fprintf(stderr, "\n");                                \
+        print_backtrace();                                    \
         exit(-1);                                             \
     } while (0)
-
-#define _force_inline_ inline __attribute__((always_inline))
 
 #endif // _HG_COMMON_H_
