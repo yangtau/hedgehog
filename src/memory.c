bcca8c271288896d88a0a311c0f282eb55b55fdd
yangtau <yanggtau@gmail.com>
2020-07-22 14:27:50 +0800

    gc mark

--- src/memory.c
+++ src/memory.c
@@ -6,7 +6,10 @@ void* hg_realloc(void* ptr, size_t old_size, size_t new_size) {
     assert(memory_usage >= old_size);
     memory_usage -= old_size;
     memory_usage += new_size;
-    return realloc(ptr, new_size);
+
+    ptr = realloc(ptr, new_size);
+
+    return ptr;
 }
 
 size_t hg_memory_usage() {
