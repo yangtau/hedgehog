4c61b55dae94ada3d927c678b26fb99511a7d274
yangtau <yanggtau@gmail.com>
2022-01-19 23:01:43 +0800

    fix string alloc bugs & add tests

--- src/string.c
+++ src/string.c
@@ -1,6 +1,8 @@
 #include "string.h"
 #include "memory.h"
 
+#include <string.h>
+
 static struct {
     hg_char* arr;
     size_t len;
@@ -28,15 +30,16 @@ void hg_string_free() {
 
 static void _string_table_append(hg_char s) {
     if (_string_table.len >= _string_table.cap) {
+        _string_table.cap *= 2;
         _string_table.arr =
-            hg_realloc(_string_table.arr, (_string_table.cap *= 2));
+            hg_realloc(_string_table.arr, sizeof(hg_char) * _string_table.cap);
     }
 
     _string_table.arr[_string_table.len++] = s;
 }
 
 hg_char hg_string_new(const char* from, size_t len) {
-    char* s = hg_alloc(sizeof(hg_char) * len + 1);
+    char* s = hg_alloc(sizeof(char) * (len + 1));
     s[len]  = '\0';
     _string_table_append(s);
 
